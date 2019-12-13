00f22d7941f343c91ab4646b111be2de8413da3f
nicolasgranata <nicolasegranata@gmail.com>
2019-12-12 21:54:33 -0300

    Adding AllYourBase

--- csharp/all-your-base/AllYourBase.cs
+++ csharp/all-your-base/AllYourBase.cs
@@ -1,9 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class AllYourBase
 {
     public static int[] Rebase(int inputBase, int[] inputDigits, int outputBase)
     {
-        throw new NotImplementedException("Please implement this function");
+        if (inputBase == 1 || inputBase <= 0 
+                || inputDigits.Any(x => x < 0 || x >= inputBase)
+                || outputBase <= 1)
+        {
+            throw new ArgumentException();
+        }
+
+        var result = new List<int>();
+
+        var tempNumber = 0;
+
+        for(int i = 0; i < inputDigits.Length; i++)
+        {
+            tempNumber += inputDigits[i] * Convert.ToInt32(Math.Pow(inputBase, inputDigits.Length - (i + 1)));
+        }
+
+        while(tempNumber > 0)
+        {
+           result.Add(tempNumber % outputBase);
+           tempNumber = tempNumber / outputBase;
+        }
+
+        return result.DefaultIfEmpty().Reverse().ToArray();
     }
 }
