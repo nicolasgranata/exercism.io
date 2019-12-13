d8f482868404f08193ba1395653681f595fa8ef7
Nico Granata <nicolasegranata@gmail.com>
2019-12-13 13:38:15 -0300

    Adding Etl

--- csharp/etl/Etl.cs
+++ csharp/etl/Etl.cs
@@ -5,6 +5,16 @@ public static class Etl
 {
     public static Dictionary<string, int> Transform(Dictionary<int, string[]> old)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var output = new Dictionary<string, int>();
+
+        foreach(var value in old)
+        {
+            foreach(var newValue in value.Value)
+            {
+                output.Add(newValue.ToLower(), value.Key);
+            }
+        }
+
+        return output;
     }
 }
