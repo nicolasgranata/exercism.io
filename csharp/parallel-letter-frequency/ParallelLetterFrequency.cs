a68c0cef0e762b19e0adec4c6cf2c781600865f0
Nico <nicolasegranata@gmail.com>
2019-12-16 20:25:15 -0300

    Adding Parallel Letter Frequency

---
+++ csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ParallelLetterFrequency
+{
+    public static Dictionary<char, int> Calculate(IEnumerable<string> texts) => 
+        texts.AsParallel().SelectMany(x => x.ToLower())
+             .Where(x => char.IsLetter(x)).GroupBy(x => x)
+             .ToDictionary(j => j.Key, x => x.Count());
+
+}
