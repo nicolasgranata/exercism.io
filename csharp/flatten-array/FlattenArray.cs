3fe547ab5530836deaffcd0b3fb70f219a546527
nicolasgranata <nicolasegranata@gmail.com>
2020-05-11 02:12:35 -0300

    Adding Flatten Array

---
+++ csharp/flatten-array/FlattenArray.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class FlattenArray
+{
+    public static IEnumerable Flatten(IEnumerable input)
+    {
+        foreach(var item in input)
+        {
+            if(item is IEnumerable)
+            {
+                foreach(var nestedItem in Flatten(item as IEnumerable))
+                {
+                    yield return nestedItem;
+                }
+            }
+            else if (item != null)
+            {
+                yield return item;
+            }
+        }
+    }
+}
